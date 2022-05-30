d21f965aa4da3687eb581d1f280ef0598122a5d2
humbertodutra <humberto.dutra@hotmail.com>
2022-05-30 20:09:01 -0300

    challenges 11-19

---
+++ desafio11.sql
@@ -0,0 +1,6 @@
+SELECT 
+    notes
+FROM
+    purchase_orders
+WHERE
+    notes LIKE 'Purchase generated based on Order #3%';
---
+++ desafio18.sql
@@ -0,0 +1,6 @@
+SELECT 
+    supplier_id
+FROM
+    purchase_orders
+WHERE
+    supplier_id = 3 AND status_id = 2;
